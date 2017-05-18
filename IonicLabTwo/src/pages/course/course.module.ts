ca8f62eaaac00f77ca11b0001c211047de2ce944
Brett Whittington <BWhittington@hmbnet.com>
2017-05-17 23:21:35 -0400

    Commit changes for partial solution

--- IonicLabTwo/src/pages/course/course.module.ts
+++ IonicLabTwo/src/pages/course/course.module.ts
@@ -1,5 +1,7 @@
 ﻿import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { FileOpener } from '@ionic-native/file-opener';
+import { FilePath } from '@ionic-native/file-path';
 
 import { CoursePage } from './course';
 import { CourseService } from '../../components/courses/shared/course.service';
@@ -8,7 +10,7 @@ import { CourseService } from '../../components/courses/shared/course.service';
     declarations: [ CoursePage ],
     imports: [ IonicPageModule.forChild(CoursePage) ],
     exports: [ CoursePage ],
-    providers: [ CourseService ]
+    providers: [CourseService, FileOpener, FilePath ]
 })
 
 export class CoursePageModule { }
--- IonicLabTwo/src/pages/course/course.ts
+++ IonicLabTwo/src/pages/course/course.ts
@@ -1,5 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FileOpener } from '@ionic-native/file-opener';
+import { FilePath } from '@ionic-native/file-path';
 
 import { CourseService } from '../../components/courses/shared/course.service';
 import { CourseModel } from '../../components/courses/shared/course.model';
@@ -12,15 +14,19 @@ import { CourseModel } from '../../components/courses/shared/course.model';
 
 export class CoursePage {
 
-    public courseModel: CourseModel;
-    public courseService: CourseService;
-    public navCtrl: NavController;
-    public navParams: NavParams;
-    public id: number;
+    private courseModel: CourseModel;
+    private courseService: CourseService;
+    private navCtrl: NavController;
+    private navParams: NavParams;
+    private id: number;
+    private fileOpener: FileOpener;
+    private filePath: FilePath;
 
-    constructor(navCtrl: NavController, navParams: NavParams, courseService: CourseService) {
+    constructor(navCtrl: NavController, navParams: NavParams, courseService: CourseService, filePath: FilePath, fileOpener: FileOpener ) {
         this.courseService = courseService;
         this.navParams = navParams;
+        this.filePath = filePath;
+        this.fileOpener = fileOpener;
 
         this.id = this.navParams.get('id');
         this.courseModel = this.courseService.getCourse(this.id);
@@ -28,5 +34,13 @@ export class CoursePage {
 
     public ionViewDidLoad(): void {
         console.log('ionViewDidLoad Course');
+    }
+
+    public openDocument(filePathString: string): void {
+        this.filePath.resolveNativePath(filePathString)
+            .then(path => this.fileOpener.open(path, 'application/pdf')
+                .then(() => console.log('File is opened'))
+                .catch(e => console.log('Error openening file', e)))
+            .catch(err => console.log(err));
     }
 }
